ff93008d3737d1ff71717e928cd3e7c68a530da5
Christhian Guevara <christhianjesus@live.com>
2023-08-14 03:29:42 -0500

    feature: refactor to apply DDD better

--- internal/application/consumption/consumption_service_test.go
+++ internal/application/consumption/consumption_service_test.go
@@ -1,11 +1,1 @@
 package consumption
-
-/* func TestGetGroupedByMetersIDs(t *testing.T) {
-	consumptionCollection := setupConsumptionCollectionTest()
-	groupedConsumption := GroupByMetersIDs(consumptionCollection, []int{1, 2})
-
-	assert.NotNil(t, groupedConsumption)
-	assert.Len(t, groupedConsumption, 2)
-	assert.NotNil(t, groupedConsumption[1])
-	assert.NotNil(t, groupedConsumption[2])
-} */
